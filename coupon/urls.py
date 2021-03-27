e6e874dc85f2b9510ee1d7b6b551d7f8018d773c
Anirudh Mittal <52218067+anirudh1117@users.noreply.github.com>
2021-03-27 19:57:58 +0530

    Delete coupon directory

--- coupon/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import check_token,Couponhistory
-
-urlpatterns = [
-    path('check-card/',check_token.as_view(),name='check'),
-    path('get-history/',Couponhistory.as_view(),name='history'),
-]
